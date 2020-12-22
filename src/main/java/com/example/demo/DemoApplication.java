46e37fb0d00c67a0c2b2ddc90e98cd296ced707d
dolinsky <martin.dolinsky@akademiasovy.sk>
2020-12-22 14:44:22 +0100

    Added TvSeries and links mappings

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class DemoApplication {
@@ -17,6 +20,15 @@ public class DemoApplication {
 	@GetMapping("/hello")
 	public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
 		return String.format("Hello %s!", name);
+	}
+
+	@GetMapping("/links")
+	public String showLinks() {
+		List<String> links = new ArrayList<>();
+		links.add("<a href=" + "http://localhost:8080/user/list" + ">users</a>");
+		links.add("<a href=" + "http://localhost:8080/movie/list" + ">movies</a>");
+		links.add("<a href=" + "http://localhost:8080/tvseries/list" + ">tvseries</a>");
+		return links.toString();
 	}
 
 
